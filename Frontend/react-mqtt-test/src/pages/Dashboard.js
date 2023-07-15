db3e610f5bfe81315feeb2d3f13e5c8a057ad689
Tran Quoc Tuan <tuantran1295@MacBook-Air-cua-Tran.local>
2023-07-15 11:32:02 +0700

    swap dashboard device page

--- Frontend/react-mqtt-test/src/pages/Dashboard.js
+++ Frontend/react-mqtt-test/src/pages/Dashboard.js
@@ -1,8 +1,8 @@
-import {useContext, useEffect, useState} from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { DataContext } from '../context/DataProvider';
 import MqttConnect from "../services/MqttConnect";
-import { useNavigate } from "react-router";
-import {TANK_TOPIC} from "../constant";
+import {useNavigate} from "react-router";
+import {TEMP_TOPIC} from "../constant";
 import Navbar from "../component/Navbar";
 
 const Dashboard = () => {
@@ -12,64 +12,54 @@ const Dashboard = () => {
 
     useEffect(() => {
         console.log(payload);
-        if (payload.topic === TANK_TOPIC) {
+        if (payload.topic === TEMP_TOPIC) {
             const parsedMessage = JSON.parse(payload.message)
             const newMessages = [...dashboardMessages, parsedMessage];
             setMessages(newMessages);
         }
-        // console.log(dashboardMessages);
     },[payload]);
 
     return <div>
         <MqttConnect/>
         <Navbar/>
-
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <table className="block max-h-screen w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     <th scope="col" className="px-6 py-3">
                         Device ID
                     </th>
                     <th scope="col" className="px-6 py-3">
-                        Device Name
+                        Sensor Name
                     </th>
                     <th scope="col" className="px-6 py-3">
-                        Status
+                        Value
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Unit
                     </th>
                 </tr>
                 </thead>
-                <tbody className="overflow-y-scroll" style={{maxHeight: "50vh"}}>
+                <tbody>
                 {dashboardMessages.map((message, index) =>
-                    <tr className="bg-white dark:bg-gray-800">
+                    <tr key={index} className="bg-white dark:bg-gray-800">
                         <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             {message.device_id}
                         </th>
                         <td className="px-6 py-4">
-                            {message.device_name}
+                            {message.sensor_name}
                         </td>
                         <td className="px-6 py-4">
-                            <div className="flex items-center">
-                                <div className={`h-2.5 w-2.5 rounded-full mr-2 ${message.status === 1 ? "bg-green-500" : "bg-red-500" } ` }></div>
-                                {message.status === 1 ? "True" : "False"}
-                            </div>
+                            {message.value}
+                        </td>
+                        <td className="px-6 py-4">
+                            {message.unit}
                         </td>
                     </tr>)
                 }
                 </tbody>
             </table>
         </div>
-
-
-        {/*<h1>DASHBOARD</h1>*/}
-        {/*<div>*/}
-        {/*    <ul className="messageList">*/}
-        {/*        {dashboardMessages.map((message, index) => <li key={index}>{JSON.stringify(message)}</li>)}*/}
-        {/*    </ul>*/}
-        {/*</div>*/}
-        {/*<button onClick={() => navigate("/device")}>*/}
-        {/*    Go to Device Page*/}
-        {/*</button>*/}
     </div>
 }
 
--- Frontend/react-mqtt-test/src/pages/Device.js
+++ Frontend/react-mqtt-test/src/pages/Device.js
@@ -1,8 +1,10 @@
-import { useContext, useEffect, useState } from 'react';
+
+
+import {useContext, useEffect, useState} from 'react';
 import { DataContext } from '../context/DataProvider';
 import MqttConnect from "../services/MqttConnect";
-import {useNavigate} from "react-router";
-import {TEMP_TOPIC} from "../constant";
+import { useNavigate } from "react-router";
+import {TANK_TOPIC} from "../constant";
 import Navbar from "../component/Navbar";
 
 const Device = () => {
@@ -12,61 +14,53 @@ const Device = () => {
 
     useEffect(() => {
         console.log(payload);
-        if (payload.topic === TEMP_TOPIC) {
+        if (payload.topic === TANK_TOPIC) {
             const parsedMessage = JSON.parse(payload.message)
             const newMessages = [...deviceMessages, parsedMessage];
             setMessages(newMessages);
         }
+        // console.log(deviceMessages);
     },[payload]);
 
     return <div>
         <MqttConnect/>
         <Navbar/>
+
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table className="block max-h-screen w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     <th scope="col" className="px-6 py-3">
                         Device ID
                     </th>
                     <th scope="col" className="px-6 py-3">
-                        Sensor Name
+                        Device Name
                     </th>
                     <th scope="col" className="px-6 py-3">
-                        Value
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                        Unit
+                        Status
                     </th>
                 </tr>
                 </thead>
-                <tbody>
+                <tbody className="overflow-y-scroll" style={{maxHeight: "50vh"}}>
                 {deviceMessages.map((message, index) =>
-                    <tr className="bg-white dark:bg-gray-800">
+                    <tr key={index} className="bg-white dark:bg-gray-800">
                         <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             {message.device_id}
                         </th>
                         <td className="px-6 py-4">
-                            {message.sensor_name}
+                            {message.device_name}
                         </td>
                         <td className="px-6 py-4">
-                            {message.value}
-                        </td>
-                        <td className="px-6 py-4">
-                            {message.unit}
+                            <div className="flex items-center">
+                                <div className={`h-2.5 w-2.5 rounded-full mr-2 ${message.status === 1 ? "bg-green-500" : "bg-red-500" } ` }></div>
+                                {message.status === 1 ? "True" : "False"}
+                            </div>
                         </td>
                     </tr>)
                 }
                 </tbody>
             </table>
         </div>
-        {/*<h1>Device</h1>*/}
-        {/*<ul className="messageList">*/}
-        {/*    {deviceMessages.map((message, index) => <li key={index}>{JSON.stringify(message)}</li>)}*/}
-        {/*</ul>*/}
-        {/*<button onClick={() => navigate("/")}>*/}
-        {/*    Go to Dashboard Page*/}
-        {/*</button>*/}
     </div>
 }
 
