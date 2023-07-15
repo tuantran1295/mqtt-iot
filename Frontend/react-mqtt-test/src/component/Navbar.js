f591aa360873267bef47fe812424870fe8978478
Tran Quoc Tuan <tuantran1295@MacBook-Air-cua-Tran.local>
2023-07-15 11:12:00 +0700

    fix table tailwind

--- Frontend/react-mqtt-test/src/component/Navbar.js
+++ Frontend/react-mqtt-test/src/component/Navbar.js
@@ -1,5 +1,9 @@
-
+import { useLocation } from 'react-router-dom';
 const Navbar = () => {
+    const location = useLocation();
+
+    const activeClassName = 'block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500';
+    const deActiveCLassName = 'block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent';
 
     return <>
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
@@ -22,12 +26,13 @@ const Navbar = () => {
                     <ul className="mr-4 font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
                             <a href="/"
-                               className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                               aria-current="page">Dashboard</a>
+                               className={location.pathname === '/' ? activeClassName : deActiveCLassName}
+                               >Dashboard</a>
                         </li>
                         <li>
                             <a href="/device"
-                               className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Device</a>
+                               className={location.pathname === '/device' ? activeClassName : deActiveCLassName}
+                            >Device</a>
                         </li>
                     </ul>
                 </div>
--- Frontend/react-mqtt-test/src/pages/Dashboard.js
+++ Frontend/react-mqtt-test/src/pages/Dashboard.js
@@ -25,7 +25,7 @@ const Dashboard = () => {
         <Navbar/>
 
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table className="max-h-screen w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     <th scope="col" className="px-6 py-3">
@@ -39,7 +39,7 @@ const Dashboard = () => {
                     </th>
                 </tr>
                 </thead>
-                <tbody>
+                <tbody className="overflow-y-scroll" style={{maxHeight: "50vh"}}>
                 {dashboardMessages.map((message, index) =>
                     <tr className="bg-white dark:bg-gray-800">
                         <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
