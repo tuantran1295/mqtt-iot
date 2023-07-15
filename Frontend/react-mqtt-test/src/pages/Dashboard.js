ac7975b9e12a456152aaa3b2dcd738fb6e8debf9
Tran Quoc Tuan <tuantran1295@MacBook-Air-cua-Tran.local>
2023-07-15 17:24:16 +0700

    add thermometer

--- Frontend/react-mqtt-test/src/pages/Dashboard.js
+++ Frontend/react-mqtt-test/src/pages/Dashboard.js
@@ -4,26 +4,30 @@ import MqttConnect from "../services/MqttConnect";
 import {useNavigate} from "react-router";
 import {TEMP_TOPIC} from "../constant";
 import Navbar from "../component/Navbar";
+import Thermometer from "react-thermometer-component";
 
 const Dashboard = () => {
     const navigate = useNavigate();
     const { payload } = useContext(DataContext);
     const [dashboardMessages, setMessages] = useState([]);
+    const [currentTemp, setCurrentTemp] = useState(0)
 
     useEffect(() => {
-        console.log(payload);
+        // console.log(payload);
         if (payload.topic === TEMP_TOPIC) {
             const parsedMessage = JSON.parse(payload.message)
             const newMessages = [...dashboardMessages, parsedMessage];
             setMessages(newMessages);
+            setCurrentTemp(parsedMessage.value);
+            console.log(parsedMessage.value);
         }
     },[payload]);
 
     return <div>
         <MqttConnect/>
         <Navbar/>
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table className="block max-h-screen w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <div style={{display: 'flex'}} className="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table className="block text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     <th scope="col" className="px-6 py-3">
@@ -59,6 +63,16 @@ const Dashboard = () => {
                 }
                 </tbody>
             </table>
+
+            <Thermometer
+                theme="light"
+                value={currentTemp}
+                max="100"
+                steps="1"
+                format="°C"
+                size="normal"
+                height="180"
+            />
         </div>
     </div>
 }
